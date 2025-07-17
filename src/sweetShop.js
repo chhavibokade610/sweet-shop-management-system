00ea9a963132fb4f0d0b46c6a88263dc04e6ea77
chhavi_bokade <chhavibokade@gmail.com>
2025-07-17 10:15:58 +0530

    Implement searchByName, searchByCategory, and searchByPriceRange methods

--- src/sweetShop.js
+++ src/sweetShop.js
@@ -18,6 +18,27 @@ class SweetShop {
   deleteSweet(id) {
     this.sweets = this.sweets.filter((sweet) => sweet.id !== id);
   }
+
+  //searches for sweets by name (case-insensitive).
+  searchByName(name) {
+    return this.sweets.filter(
+      (sweet) => sweet.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+
+  //search for the sweets by category (case - insensitive)
+  searchByCategory(category) {
+    return this.sweets.filter(
+      (sweet) => sweet.category.toLowerCase() === category.toLowerCase()
+    );
+  }
+
+  //Search for the sweets by price range
+  searchByPriceRange(min, max) {
+    return this.sweets.filter(
+      (sweet) => sweet.price >= min && sweet.price <= max
+    );
+  }
 }
 
 module.exports = SweetShop;
